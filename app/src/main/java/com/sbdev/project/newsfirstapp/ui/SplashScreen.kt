7c45f7c328921b00e0c7d103a16ebc024a3da5e0
sb-android-dev <bhanvadiasmit28@gmail.com>
2022-03-10 20:42:33 +0530

    Initial commit

---
+++ app/src/main/java/com/sbdev/project/newsfirstapp/ui/SplashScreen.kt
@@ -0,0 +1,14 @@
+package com.sbdev.project.newsfirstapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
